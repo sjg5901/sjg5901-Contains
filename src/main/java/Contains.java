108722f2f1c3af1829efe2baf24e52320a75d747
Skyler Giannetti <sjg5901@psu.edu>
2023-07-14 20:35:07 +0000

    Contains challenge complete

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -11,6 +11,10 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
+        
+        for (int num : arr) { 
+            if (num == target) return true; 
+        };
         return false;
     }
 }
